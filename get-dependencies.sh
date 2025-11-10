5af2b4c11f184636ebb0500894d9dfcd86b41112
kenderipa <52325831+kenderipa@users.noreply.github.com>
2025-11-10 21:55:38 +0100

    Comment out debloated package installation
    Comment out the installation of debloated packages and related echo statements.

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -19,7 +19,7 @@ pacman -Syu --noconfirm   \
 	qt5-base		      \
 	wget                  \
 	xorg-server-xvfb      \
-	zsync                 \
+	zsync                 #\
 #    boost-libs            \
 #    desktop-file-utils    \
 #    ffmpeg                \
@@ -40,11 +40,11 @@ pacman -Syu --noconfirm   \
 
 	
 
-echo "Installing debloated packages..."
-echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
-chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-opengl --prefer-nano opus-mini
+#echo "Installing debloated packages..."
+#echo "---------------------------------------------------------------"
+#wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
+#chmod +x ./get-debloated-pkgs.sh
+#./get-debloated-pkgs.sh --add-opengl --prefer-nano opus-mini
 
 echo "Building touchhle..."
 echo "---------------------------------------------------------------"
