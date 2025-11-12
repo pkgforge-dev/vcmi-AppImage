f1164783da7fa8d7f6dea284fec650f859f2350c
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-11 23:03:24 -0400

    simplify

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,7 +9,6 @@ echo "Installing build dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
 	base-devel          \
-	boost				\
 	cmake				\
 	curl                \
 	git                 \
@@ -30,7 +29,7 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-opengl --prefer-nano opus-mini
+./get-debloated-pkgs.sh --add-common --prefer-nano
 
 echo "Building vcmi..."
 echo "---------------------------------------------------------------"
