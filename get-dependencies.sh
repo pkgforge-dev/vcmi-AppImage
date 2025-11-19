cc357983944725d26b016e16010c468bb9285639
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-19 11:20:03 -0400

    simplify with new template (#4)

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -2,38 +2,13 @@
 
 set -eu
 
-EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
-PACKAGE_BUILDER="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/make-aur-package.sh"
-
-echo "Installing build dependencies..."
-echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-	base-devel          \
-	cmake				\
-	curl                \
-	git                 \
-	libx11              \
-	libxrandr           \
-	libxss              \
-	minizip				\
-	ninja				\
-	pulseaudio          \
-	pulseaudio-alsa     \
-	qt6-tools		    \
-	wget                \
-	xorg-server-xvfb    \
-	zsync
+#echo "Installing package dependencies..."
+#echo "---------------------------------------------------------------"
+# pacman -Syu --noconfirm PACKAGESHERE
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
-chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano
 
-echo "Building vcmi..."
-echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$PACKAGE_BUILDER" -O ./make-aur-package.sh
-chmod +x ./make-aur-package.sh
-./make-aur-package.sh --chaotic-aur vcmi
-
-pacman -Q vcmi | awk '{print $2; exit}' > ~/version
+# Comment this out if you need an AUR package
+make-aur-package --chaotic-aur vcmi
---
+++ make-appimage.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -eu
+
+ARCH=$(uname -m)
+VERSION=$(pacman -Q vcmi | awk '{print $2; exit}') # example command to get version of application here
+export ADD_HOOKS="self-updater.bg.hook"
+export OUTPATH=./dist
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export DESKTOP=/usr/share/applications/vcmiclient.desktop
+export ICON=/usr/share/icons/hicolor/scalable/apps/vcmiclient.svg
+# app is hardcoded in multiple places
+export PATH_MAPPING_HARDCODED='*vcmi*'
+
+# Deploy dependencies
+quick-sharun \
+	/usr/bin/vcmi*  \
+	/usr/lib/vcmi/* \
+	/usr/lib/vcmi/*/*
+
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
